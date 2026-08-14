2b15ae41305635a784a7af7954670bcd7d41896c
mohitkumar7895 <mohitporwal596@gmail.com>
2026-08-14 15:05:57 +0530

    Fix Tailwind CSS warnings

---
+++ scratch/fix_tailwind_classes.mjs
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
+function walkDir(dir, callback) {
+  fs.readdirSync(dir).forEach(f => {
+    let dirPath = path.join(dir, f);
+    let isDirectory = fs.statSync(dirPath).isDirectory();
+    isDirectory ? walkDir(dirPath, callback) : callback(dirPath);
+  });
+}
+
+function processFile(filePath) {
+  if (!filePath.endsWith('.tsx') && !filePath.endsWith('.ts')) return;
+  let content = fs.readFileSync(filePath, 'utf8');
+  let original = content;
+  
+  content = content.replaceAll("aspect-16/9", "aspect-video");
+  content = content.replaceAll("z-[80]", "z-80");
+  content = content.replaceAll("break-words", "wrap-break-word");
+  content = content.replaceAll("p-[1px]", "p-px");
+  content = content.replaceAll("aspect-[4/3]", "aspect-4/3");
+  content = content.replaceAll("lg:h-[4.25rem]", "lg:h-17");
+  
+  if (original !== content) {
+    fs.writeFileSync(filePath, content, 'utf8');
+    console.log(`Updated ${filePath}`);
+  }
+}
+
+walkDir('app', processFile);
+walkDir('components', processFile);
